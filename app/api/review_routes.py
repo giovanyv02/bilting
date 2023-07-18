dfffc8de536d6f02269674b0a01e5b6964e31388
giovany <victorgiovany02@gmail.com>
2023-07-18 06:59:33 -0400

    ,

--- app/api/review_routes.py
+++ app/api/review_routes.py
@@ -2,7 +2,7 @@ from flask import Blueprint, request
 from flask_login import login_required, current_user
 from app.models import Review, db
 from app.forms import ReviewForm
-from .AWS_helpers import get_unique_filename, upload_file_to_s3, remove_file_from_s3
+# from .AWS_helpers import get_unique_filename, upload_file_to_s3, remove_file_from_s3
 
 review_routes = Blueprint('reviews', __name__)
 
@@ -26,17 +26,17 @@ def addReview():
     data = form.data
     print("mennn data", data)
     if form.validate_on_submit():
-        image = data['image']
-        image.filename = get_unique_filename(image.filename)
-        upload = upload_file_to_s3(image)
-        print(upload)
+        # image = data['image']
+        # image.filename = get_unique_filename(image.filename)
+        # upload = upload_file_to_s3(image)
+        # print(upload)
 
         newReview = Review(
             user_id = data.current_user.id,
             item_id = data['item_id'],
             review = data['review'],
             stars = data['stars'],
-            image = upload['url']
+            # image = upload['url']
 
         )
         db.session.add(newReview)
