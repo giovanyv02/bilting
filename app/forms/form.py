92cfc4f801f859fdaecb01ff203d087fb2088c8a
giovany <victorgiovany02@gmail.com>
2023-07-18 07:46:02 -0400

    ,

--- app/forms/form.py
+++ app/forms/form.py
@@ -2,7 +2,6 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, IntegerField
 from wtforms.validators import DataRequired
 from flask_wtf.file import FileField, FileAllowed, FileRequired
-from api.AWS_helpers import ALLOWED_EXTENSIONS
 from app.api.AWS_helpers import ALLOWED_EXTENSIONS
 
 class ReviewForm(FlaskForm):
