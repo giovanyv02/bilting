902b0f914dd563ecca1cff4c9d759a3e54558f28
giovany <victorgiovany02@gmail.com>
2023-07-21 12:09:17 -0400

    style delete review

--- react-app/src/components/Review/DeleteReview/index.js
+++ react-app/src/components/Review/DeleteReview/index.js
@@ -1,6 +1,7 @@
 import { deleteReview } from "../../../store/review";
 import { useDispatch, useSelector} from "react-redux";
 import { useModal } from "../../../context/Modal";
+import "./deleteReview.css"
 
 export default function DeleteReview({id}){
     const { closeModal } = useModal();
@@ -23,8 +24,8 @@ export default function DeleteReview({id}){
         <>
         <div className="delDiv">
 
-        <h3 className="delH3">Confirm Delete</h3>
-        <p className="delP"> Remove this review?</p>
+        <h3 className="delH3">Delete</h3>
+        <p className="delP"> Remove this review</p>
         <button onClick={subDelete} className="delYes">Continue</button>
         <button onClick={subNo} type="submit" className="delNo">Cancel</button>
         </div>
