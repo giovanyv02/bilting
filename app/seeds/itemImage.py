512d9305dbc0034c57d465049a39c9a3ba9cffbf
giovany <victorgiovany02@gmail.com>
2023-07-23 16:20:35 -0400

    add more seed data

--- app/seeds/itemImage.py
+++ app/seeds/itemImage.py
@@ -28,12 +28,93 @@ def seed_itemImages():
           item_id = 6, url = 'https://cdn.discordapp.com/attachments/1130957424296198224/1130966631129022605/item6.png'
        
     )
+    itemImage7 = ItemImage(
+        item_id = 7, url = 'https://cdn.discordapp.com/attachments/1130957424296198224/1132694877327593572/football6.png'
+    )
+    itemImage8 = ItemImage(
+        item_id = 8, url = 'https://cdn.discordapp.com/attachments/1130957424296198224/1132694877608628316/football5.png'
+    )
+    itemImage9 = ItemImage(
+        item_id = 9, url = 'https://cdn.discordapp.com/attachments/1130957424296198224/1132694878120316948/football4.png'
+    )
+    itemImage10 = ItemImage(
+        item_id = 10, url = 'https://cdn.discordapp.com/attachments/1130957424296198224/1132694878539743383/football3.png'
+    )
+    itemImage11 = ItemImage(
+        item_id = 11, url = 'https://cdn.discordapp.com/attachments/1130957424296198224/1132694878791422033/football2.png'
+    )
+    itemImage12 = ItemImage(
+        item_id = 12, url = 'https://cdn.discordapp.com/attachments/1130957424296198224/1132694879005315122/football1.png'
+    )
+    itemImage13 = ItemImage(
+        item_id = 13, url = 'https://cdn.discordapp.com/attachments/1130957424296198224/1132694879240192000/basket8.png'
+    )
+    itemImage14 = ItemImage(
+        item_id = 14, url = 'https://cdn.discordapp.com/attachments/1130957424296198224/1132694879487660042/basket7.png'
+    )
+    itemImage15 = ItemImage(
+        item_id = 15, url = 'https://cdn.discordapp.com/attachments/1130957424296198224/1132694879743508501/basket6.png'
+    )
+    itemImage16 = ItemImage(
+        item_id = 16, url = 'https://cdn.discordapp.com/attachments/1130957424296198224/1132694879986798613/basket5.png'
+    )
+    itemImage17 = ItemImage(
+        item_id = 17, url = 'https://cdn.discordapp.com/attachments/1130957424296198224/1132695002028441772/basket4.png'
+    )
+    itemImage18 = ItemImage(
+        item_id = 18, url = 'https://cdn.discordapp.com/attachments/1130957424296198224/1132695002376589412/basket3.png'
+    )
+    itemImage19 = ItemImage(
+        item_id = 19, url = 'https://cdn.discordapp.com/attachments/1130957424296198224/1132695002645012621/basket2.png'
+    )
+    itemImage20 = ItemImage(
+        item_id = 20, url = 'https://cdn.discordapp.com/attachments/1130957424296198224/1132695003064434718/basket1.png'
+    )
+    itemImage21 = ItemImage(
+        item_id = 21, url = 'https://cdn.discordapp.com/attachments/1130957424296198224/1132695003286753320/golf4.png'
+    )
+    itemImage22 = ItemImage(
+        item_id = 22, url = 'https://cdn.discordapp.com/attachments/1130957424296198224/1132695003521622026/golf3.png'
+    )
+    itemImage23 = ItemImage(
+        item_id = 23, url = 'https://cdn.discordapp.com/attachments/1130957424296198224/1132695003743912057/golf2.png'
+    )
+    itemImage24 = ItemImage(
+        item_id = 24, url = 'https://cdn.discordapp.com/attachments/1130957424296198224/1132695004113031219/golf1.png'
+    )
+    itemImage25 = ItemImage(
+        item_id = 25, url = 'https://cdn.discordapp.com/attachments/1130957424296198224/1132695004444368946/soccer6.png'
+    )
+    itemImage26 = ItemImage(
+        item_id = 26, url = 'https://cdn.discordapp.com/attachments/1130957424296198224/1132695004696018995/soccer5.png'
+    )
+    
     db.session.add(itemImage1)
     db.session.add(itemImage2)
     db.session.add(itemImage3)
     db.session.add(itemImage4)
     db.session.add(itemImage5)
     db.session.add(itemImage6)
+    db.session.add(itemImage7)
+    db.session.add(itemImage8)
+    db.session.add(itemImage9)
+    db.session.add(itemImage10)
+    db.session.add(itemImage11)
+    db.session.add(itemImage12)
+    db.session.add(itemImage13)
+    db.session.add(itemImage14)
+    db.session.add(itemImage15)
+    db.session.add(itemImage16)
+    db.session.add(itemImage17)
+    db.session.add(itemImage18)
+    db.session.add(itemImage19)
+    db.session.add(itemImage20)
+    db.session.add(itemImage21)
+    db.session.add(itemImage22)
+    db.session.add(itemImage23)
+    db.session.add(itemImage24)
+    db.session.add(itemImage25)
+    db.session.add(itemImage26)
     db.session.commit()
 
 
--- app/seeds/items.py
+++ app/seeds/items.py
@@ -27,12 +27,93 @@ def seed_items():
         sex='kid', category='shirt', featured='fitness essntial', sports='basket', price=40
        
     )
+    item7 = Item(
+        sex='man', category='pants', featured='new arrival', sports='football', price=2
+    )
+    item8 = Item(
+        sex='man', category='shirt', featured='new arrival', sports='football', price=145
+    )
+    item9 = Item(
+        sex='man', category='shirt', featured='new arrival', sports='football', price=1409
+    )
+    item10 = Item(
+        sex='women', category='shoe', featured='new arrival', sports='football', price=99
+    )
+    item11 = Item(
+        sex='man', category='shoe', featured='new arrival', sports='football', price=149
+    )
+    item12 = Item(
+        sex='man', category='short', featured='new arrival', sports='football', price=66
+    )
+    item13 = Item(
+        sex='man', category='shoe', featured='new arrival', sports='basket', price=22
+    )
+    item14 = Item(
+        sex='man', category='shoe', featured='new arrival', sports='basket', price=14
+    )
+    item15 = Item(
+        sex='women', category='pant', featured='new arrival', sports='basket', price=99
+    )
+    item16 = Item(
+        sex='women', category='shirt', featured='new arrival', sports='basket', price=87
+    )
+    item17 = Item(
+        sex='man', category='shoe', featured='new arrival', sports='basket', price=11
+    )
+    item18 = Item(
+       sex='man', category='shoe', featured='new arrival', sports='basket', price=2996
+    )
+    item19 = Item(
+        sex='man', category='shoe', featured='new arrival', sports='basket', price=32
+    )
+    item20 = Item(
+        sex='women', category='shoe', featured='new arrival', sports='basket', price=1409
+    )
+    item21 = Item(
+        sex='man', category='bag', featured='new arrival', sports='golf', price=34
+    )
+    item22 = Item(
+        sex='women', category='shoe', featured='new arrival', sports='golf', price=2987
+    )
+    item23 = Item(
+        sex='man', category='shirt', featured='new arrival', sports='golf', price=196
+    )
+    item24 = Item(
+        sex='man', category='pant', featured='new arrival', sports='football', price=6789
+    )
+    item25 = Item(
+        sex='man', category='shoe', featured='new arrival', sports='soccer', price=839
+    )
+    item26 = Item(
+        sex='women', category='short', featured='new arrival', sports='soccer', price=8543
+    )
+
     db.session.add(item1)
     db.session.add(item2)
     db.session.add(item3)
     db.session.add(item4)
     db.session.add(item5)
     db.session.add(item6)
+    db.session.add(item7)
+    db.session.add(item8)
+    db.session.add(item9)
+    db.session.add(item10)
+    db.session.add(item11)
+    db.session.add(item12)
+    db.session.add(item13)
+    db.session.add(item14)
+    db.session.add(item15)
+    db.session.add(item16)
+    db.session.add(item17)
+    db.session.add(item18)
+    db.session.add(item19)
+    db.session.add(item20)
+    db.session.add(item21)
+    db.session.add(item22)
+    db.session.add(item23)
+    db.session.add(item24)
+    db.session.add(item25)
+    db.session.add(item26)
     db.session.commit()
 
 
