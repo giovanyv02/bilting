26a4003f62a4e3fe1f110296445cd9e5da32d087
giovany <victorgiovany02@gmail.com>
2023-07-27 17:10:12 -0400

    add redux for fav

---
+++ app/seeds/favorite.py
@@ -0,0 +1,34 @@
+from app.models import db, Favorite, environment, SCHEMA
+from sqlalchemy.sql import text
+
+
+# Adds a demo user, you can add other users here if you want
+def seed_favorites():
+    favorite1 = Favorite(
+        user_id = 1
+    )
+    favorite2 = Favorite(
+         user_id = 2
+        
+    )
+    favorite3 = Favorite(
+         user_id = 3
+        
+    )
+  
+  
+    db.session.add(favorite1)
+    db.session.add(favorite2)
+    db.session.add(favorite3)
+    db.session.commit()
+
+
+
+def undo_favorites():
+    if environment == "production":
+        db.session.execute(
+            f"TRUNCATE table {SCHEMA}.carts RESTART IDENTITY CASCADE;")
+    else:
+        db.session.execute(text("DELETE FROM favorites"))
+
+    db.session.commit()
